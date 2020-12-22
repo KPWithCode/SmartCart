58bdf6af3a172c8ba20c12d1566af8042c1cc90c
Kyle Phillips <kphillips375@yahoo.com>
2020-12-22 13:05:30 -0500

    get and post items

---
+++ backend/routes/items.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+
+let Item = require('../models/itemmodel');
+
+
+router.get('/', async (req, res) => {
+    try {
+        const getItems = await Item.find();
+        res.json(getItems)
+    } catch (err) {
+        console.log('error', err)
+    }
+});
+
+
+router.post('/add', async (req, res) => {
+    const newItem = new Item({
+        name: req.body.name,
+        category: req.body.category,
+        price: Number(req.body.price)
+    });
+    try {
+        const addItem = await newItem.save()
+        res.json(addItem);
+    } catch (err) {
+        res.sendStatus(500)
+    }
+});
+
+
+module.exports = router;
