f83cb056f31962020782cfc54cf864a3ed1c3e65
Kyle Phillips <kphillips375@yahoo.com>
2020-12-22 16:34:00 -0500

    Homepage fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './pages/Home/Home';
-import Shop from './pages/Shop';
 import Navbar from './components/Nav/Navbar';
 
 const App = () => {
@@ -11,7 +10,6 @@ const App = () => {
         <Navbar />
           <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/shop" component={Shop} />
           </Switch>
         </Router>
       </BrowserRouter>
