69d297b0b920ae6cd813cc1882784497945e8900
Kyle Phillips <kphillips375@yahoo.com>
2020-12-22 18:38:22 -0500

    Improved UI

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.css';
 import garden from '../../assets/gardengirl.jpeg';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 
 const Home = () => {
@@ -9,13 +10,26 @@ const Home = () => {
     return (
         <div className="main">
             <div className="container">
-                <img className="background-img" src={garden} />
+                <img className="background-img" src={garden} alt="flower girl" />
                 <div>
-                <h1 className="title">SmartCart</h1>
+                    <h1 className="title">SmartCart</h1>
                 </div>
                 <div>
-                <h1 className="subtitle">Shop Now</h1>
-                    <Link to="/shop"><ArrowDownwardIcon style={{color: '#459922', fontSize: '6em'}}/></Link>
+                    <h1 className="subtitle">Shop Now</h1>
+                    <motion.div
+                        animate={{
+                            scale: [.7, 1.23, 1.23, 1, .7],
+                            rotate: [0, 0, 360, 360, 0],
+                        }}
+                        transition={{
+                            duration: 2,
+                            ease: "easeInOut",
+                            times: [0, 0.2, 0.5, 0.8, 1],
+                            loop: Infinity,
+                            repeatDelay: 3
+                        }}>
+                        <Link to="/shop"><ArrowDownwardIcon style={{ color: '#063301', fontSize: '6em' }} /></Link>
+                    </motion.div>
                 </div>
             </div>
         </div>
