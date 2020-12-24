9aee32218265b09c174dcb47f1fd4da272ace0b9
Kyle Phillips <kphillips375@yahoo.com>
2020-12-23 19:48:37 -0500

    Touching up UI

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -20,45 +20,42 @@ const Home = () => {
             }
         },
         exit: {
-         y: '-100vh',
-         transition: {
-             ease: 'easeOut',
-             duration: 1
-         }
-        } 
+            y: '-100vh',
+            transition: {
+                ease: 'easeOut',
+                duration: 1
+            }
+        }
     };
 
     return (
-            <motion.div className="main"
-                variants={pageVariants}
-                initial= "hidden"
-                animate="visible"
-                exit="exit"
-            >
-                <div className="container">
-                    <img className="background-img" src={garden} alt="flower girl" />
-                    <div>
-                        <h1 className="title">SmartCart</h1>
-                    </div>
-                    <div>
-                        <h1 className="subtitle">Shop Now</h1>
-                        <motion.div
-                            animate={{
-                                scale: [.7, 1.23, 1.23, 1, .7],
-                                rotate: [0, 0, 360, 360, 0],
-                            }}
-                            transition={{
-                                duration: 2,
-                                ease: "easeInOut",
-                                times: [0, 0.2, 0.5, 0.8, 1],
-                                loop: Infinity,
-                                repeatDelay: 3
-                            }}>
-                            <Link to="/shop"><ArrowDownwardIcon style={{ color: '#063301', fontSize: '6em' }} /></Link>
-                        </motion.div>
-                    </div>
+        <motion.div className="main"
+            variants={pageVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+        >
+            <div className="container">
+                <h1 className="title">SmartCart</h1>
+                <img className="background-img" src={garden} alt="flower girl" />
+                <div>
+                    <motion.div
+                        animate={{
+                            scale: [.7, 1.23, 1.23, 1, .7],
+                            rotate: [0, 0, 360, 360, 0],
+                        }}
+                        transition={{
+                            duration: 2,
+                            ease: "easeInOut",
+                            times: [0, 0.2, 0.5, 0.8, 1],
+                            loop: Infinity,
+                            repeatDelay: 3
+                        }}>
+                        <Link to="/shop"><ArrowDownwardIcon style={{ color: '#063301', fontSize: '8em' }} /></Link>
+                    </motion.div>
                 </div>
-            </motion.div>
+            </div>
+        </motion.div>
     )
 };
 
