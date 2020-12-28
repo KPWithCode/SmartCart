458ffb6ffbf0fe1a960b8c5735645e71c84f8fc0
Kyle Phillips <kphillips375@yahoo.com>
2020-12-27 22:07:08 -0500

    UI updated to display item contents appropriately

--- frontend/src/features/items/Item.js
+++ frontend/src/features/items/Item.js
@@ -1,30 +1,27 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { unwrapResult } from '@reduxjs/toolkit';
 import { getItems } from './itemSlice';
 import { motion } from 'framer-motion';
 import './Item.css';
 
 const Items = () => {
-    const {items} = useSelector(state => state.items);
+    const { items } = useSelector(state => state.items);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(getItems({ limit: 6 }))
+        dispatch(getItems({ limit: 6 }));
     }, [dispatch]);
 
 
     const itemVariants = {
         hidden: {
-            opacity: 0,
-            y: '100vw'
+            opacity: 0
         },
         visible: {
             opacity: 1,
-            y: 0,
             transition: {
-                ease: 'easeIn',
-                duration: 2
+                delay: 1,
+                duration: 1.2
             }
         },
         exit: {
@@ -43,22 +40,19 @@ const Items = () => {
             animate="visible"
             exit="exit"
         >
-            <div className="container-shop">
+            <div className="container-products">
                 {curatedItems && curatedItems.map(cur => {
-                console.log('lll', items)
                     return (
-                        <div key={cur._id} className="item-shop">
-                        <h3 className="item-name">{cur.name}</h3>
+                        <div key={cur._id} className="item-card">
+                            <div className="">
+                                <img className="item-image" src={cur.image} />
+                                <h4 className="item-name">{cur.name}</h4>
+                            </div>
+                            <button className="button">Add</button>
                         </div>
 
                     )
                 })}
-                {/* <div className="item-shop"><h5 className="item-name">{items.name}</h5></div>
-                <div className="item-shop"></div>
-                <div className="item-shop"></div>
-                <div className="item-shop"></div>
-                <div className="item-shop"></div>
-                <div className="item-shop"></div> */}
             </div>
             <div className="cart-container"></div>
         </motion.div>
