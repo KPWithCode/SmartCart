4d1efcfd48b9a4a4f1abb0566cadedfd7e10ba2a
Kyle Phillips <kphillips375@yahoo.com>
2020-12-29 02:31:06 -0500

    Updated counter increment/decrement logic

--- frontend/src/features/counter/Counter.js
+++ frontend/src/features/counter/Counter.js
@@ -3,59 +3,25 @@ import { useSelector, useDispatch } from 'react-redux';
 import {
   decrement,
   increment,
-  incrementByAmount,
-  incrementAsync,
   selectCount,
 } from './counterSlice';
-import styles from './Counter.module.css';
+import AddBoxOutlinedIcon from '@material-ui/icons/AddBoxOutlined';
+import IndeterminateCheckBoxOutlinedIcon from '@material-ui/icons/IndeterminateCheckBoxOutlined';
 
-export function Counter() {
+const Counter = () => {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
 
   return (
-    <div>
-      <div className={styles.row}>
-      <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={e => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() =>
-            dispatch(incrementByAmount(Number(incrementAmount) || 0))
-          }
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
-        >
-          Add Async
-        </button>
-      </div>
-    </div>
+    <div className="increment">
+    <span onClick={() => dispatch(decrement())}>
+    <IndeterminateCheckBoxOutlinedIcon />
+    </span>
+    <span onClick={() => dispatch(increment())} >
+    <AddBoxOutlinedIcon />  
+    </span>
+</div>
   );
 }
+
+export default Counter;
